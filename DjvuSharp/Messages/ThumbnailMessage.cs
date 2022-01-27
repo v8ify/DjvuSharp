6404d8a2efe2798d7764847b4c1ee709c7316f55
Prajwal-Jadhav <jadhavv933@gmail.com>
2022-01-27 18:41:02 +0530

    Create ThumbnailMessage.cs

---
+++ DjvuSharp/Messages/ThumbnailMessage.cs
@@ -0,0 +1,30 @@
+﻿/*
+*   DjvuSharp - .NET bindings for DjvuLibre
+*   Copyright (C) 2021 Prajwal Jadhav
+*   
+*   This program is free software; you can redistribute it and/or
+*   modify it under the terms of the GNU General Public License
+*   as published by the Free Software Foundation; either version 2
+*   of the License, or (at your option) any later version.
+*   
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*   
+*   You should have received a copy of the GNU General Public License
+*   along with this program; if not, write to the Free Software
+*   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+using System.Runtime.InteropServices;
+
+namespace DjvuSharp.Messages
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public class ThumbnailMessage
+    {
+        public AnyMessage Any;
+        public int PageNo;
+    }
+}
