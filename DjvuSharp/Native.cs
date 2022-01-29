718cb992c6f813e82c8a126e74ee59d85d273498
Prajwal-Jadhav <jadhavv933@gmail.com>
2022-01-29 10:49:12 +0530

    Change entry points of some pinvoke methods.
    Since, we use some native methods which are actually just C function macros,
    they are not present in the dll as functions.
    
    Hence, care should be taken to use only functions not macros, lest we get
    DllEntryPointNotFound exception

--- DjvuSharp/Native.cs
+++ DjvuSharp/Native.cs
@@ -381,10 +381,10 @@ namespace DjvuSharp
         [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
         internal extern static IntPtr ddjvu_document_job(IntPtr document);
 
-        [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(dllname, CallingConvention = CallingConvention.Cdecl, EntryPoint = "ddjvu_job_release")]
         internal extern static void ddjvu_document_release(IntPtr document);
 
-        [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(dllname, CallingConvention = CallingConvention.Cdecl, EntryPoint = "ddjvu_job_status")]
         internal extern static JobStatus ddjvu_document_decoding_status(IntPtr document);
 
         [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
@@ -469,7 +469,7 @@ namespace DjvuSharp
         /// or thread needs it.
         /// </summary>
         /// <param name="page"></param>
-        [DllImport(dllname, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(dllname, CallingConvention = CallingConvention.Cdecl, EntryPoint = "ddjvu_job_release")]
         internal extern static void ddjvu_page_release(IntPtr page);
 
         /// <summary>
