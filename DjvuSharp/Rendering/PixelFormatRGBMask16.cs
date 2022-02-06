4745e52fde2be7a292044d908250f4c7af8cf186
Prajwal-Jadhav <jadhavv933@gmail.com>
2022-02-06 17:49:09 +0530

    Create PixelFormatRGBMask16 class

---
+++ DjvuSharp/Rendering/PixelFormatRGBMask16.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DjvuSharp.Enums;
+
+namespace DjvuSharp.Rendering
+{
+    public class PixelFormatRGBMask16: PixelFormat
+    {
+        public PixelFormatRGBMask16(int redMask, int greenMask, int blueMask, int XORValue = 0) : base()
+        {
+            _bpp = 16;
+
+            if (redMask < 0 || greenMask < 0 || blueMask < 0 || XORValue < 0)
+            {
+                throw new ArgumentException("Arguments must not be negative");
+            }
+
+            redMask &= 0xFFFF;
+            greenMask &= 0xFFFF;
+            blueMask &= 0xFFFF;
+            XORValue &= 0xFFFF;
+
+            int[] args = new int[4] { redMask, greenMask, blueMask, XORValue };
+
+            _djvu_format = Native.ddjvu_format_create(PixelFormatStyle.RGBMASK16, 4, args);
+
+            if (_djvu_format == IntPtr.Zero)
+            {
+                throw new ApplicationException($"Failed to create {nameof(PixelFormatRGBMask16)}");
+            }
+        }
+    }
+}
