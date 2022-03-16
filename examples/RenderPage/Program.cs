9b9629b77e38c188e5585a779066973c63864eaf
Prajwal-Jadhav <jadhavv933@gmail.com>
2022-03-16 12:13:43 +0530

    Create a new project as an example for how to render an image

---
+++ examples/RenderPage/Program.cs
@@ -0,0 +1,22 @@
+﻿using DjvuSharp;
+using DjvuSharp.Enums;
+using DjvuSharp.Rendering;
+
+const string djvuFile = @"C:\Users\prajwalj\Desktop\DjvuSharp\assets\boy_and_chicken.djvu";
+const string targetFile = @"C:\Users\prajwalj\Desktop\chicken.ppm";
+
+DjvuDocument document = DjvuDocument.Create(djvuFile);
+
+DjvuPage page = new DjvuPage(document, 1);
+
+Rectangle rect = new Rectangle(0, 0, page.Width, page.Height);
+
+byte[] imagePixels = page.Render(RenderMode.COLOR, rect, rect, new PixelFormatGrey());
+
+using (BinaryWriter bw = new BinaryWriter(File.Open(targetFile, FileMode.OpenOrCreate)))
+{
+    foreach (byte i in imagePixels)
+    {
+        bw.Write(i);
+    }
+}
